d95838598a8502a8889f3d4e553bfbaad7cd6a9c
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-08-13 10:01:20 +0800

    Setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Pages from "./pages";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Pages/>
   </React.StrictMode>,
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>
+    }
+]);
+
+export default function Pages() {
+    return <RouterProvider router={router} />
+}
